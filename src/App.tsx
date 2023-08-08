d6e7c47ba2889a582d7a6a20a3af6442a72261fa
yoshiishunichi <syun_syun01@icloud.com>
2023-08-09 03:18:09 +0900

    :art: stylelintとsassの導入

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import reactLogo from "./assets/react.svg";
 
 import viteLogo from "/vite.svg";
 
-import "./App.css";
+import "./App.scss";
 
 function App() {
   const [count, setCount] = useState(0);
